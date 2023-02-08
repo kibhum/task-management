b4c809d183d4e13f468878e95d82755ec72eafa9
kibethh <kibethh@gmail.com>
2023-02-08 13:32:51 +0300

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'sqlkibethh',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
+})
+export class AppModule {}
